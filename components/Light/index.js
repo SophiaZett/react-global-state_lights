09aa75e6ee1108333a6184be7ca250eb065757f5
Sophia S. <sophia0791@icloud.com>
2024-01-18 14:43:04 +0100

    code entgegner

--- components/Light/index.js
+++ components/Light/index.js
@@ -1,21 +1,15 @@
 import { useState } from "react";
 import { LightButton, Icon, Text, Name, State } from "./Light.styled";
 
-export default function Light({ name }) {
-  const [isOn, setIsOn] = useState(false);
+export default function Light({ name, handleIsOn, lights, isOn, id }) {
+  // const [isOn, setIsOn] = useState(false);
 
-  function handleToggle() {
-    setIsOn((isOn) => !isOn);
-  }
-
+  // function handleToggle() {
+  //   setIsOn((isOn) => !isOn);
+  // }
+  console.log(lights);
   return (
-    <LightButton
-      type="button"
-      onClick={() => {
-        handleToggle();
-      }}
-      $isOn={isOn}
-    >
+    <LightButton type="button" onClick={() => handleIsOn(id)} $isOn={isOn}>
       <Icon $isOn={isOn}>💡</Icon>
       <Text>
         <Name>{name}</Name>
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,38 @@
 import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
+import { useState } from "react";
+
+const initialLights = [
+  { id: "1", name: "Living Room", isOn: true },
+  { id: "2", name: "Kitchen", isOn: true },
+  { id: "3", name: "Bedroom", isOn: true },
+  { id: "4", name: "Bathroom", isOn: true },
+  { id: "5", name: "Garage", isOn: true },
+  { id: "6", name: "Porch", isOn: true },
+  { id: "7", name: "Garden", isOn: true },
+  { id: "8", name: "Office", isOn: true },
+];
 
 export default function App({ Component, pageProps }) {
+  const [lights, setLights] = useState(initialLights);
+
+  function handleIsOn(lightID) {
+    setLights(
+      lights.map((light) =>
+        light.id === lightID ? { ...light, isOn: !light.isOn } : light
+      )
+    );
+  }
+
   return (
     <Layout>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component
+        {...pageProps}
+        handleIsOn={handleIsOn}
+        lights={lights}
+        id={lights.id}
+      />
     </Layout>
   );
 }
